95fa06f999650703afb12adfdaf6e024e5c4a894
peterfoxflick <peterfoxflick@gmail.com>
2020-06-03 07:30:59 -0400

    Initial Commit

---
+++ Swatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swatch
+//
+//  Created by Peter Flickinger on 6/3/20.
+//  Copyright © 2020 Peter Flickinger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
